e9a33d59f55790ed7838a7644473e2ba2c12dd45
William Pittman <wspittman@gmail.com>
2024-04-10 11:05:05 -0700

    Scaffolding

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@typescript-eslint',
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	rules: {
+		'@typescript-eslint/no-unused-vars': ['error', { 'ignoreRestSiblings': true }],
+		'@typescript-eslint/space-before-function-paren': ['error', 'never']
+	},
+	parserOptions: {
+		'project': './tsconfig.json'
+	},
+};
