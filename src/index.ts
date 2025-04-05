3f1503f98c3d43432598caa2d5d810d239a110f7
William Pittman <wspittman@gmail.com>
2025-04-05 16:28:27 -0700

    Help message

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import { coins } from './coins';
 import { price } from './price';
 import { closeInput, getCommand } from './userInput';
 
+/**
+ * Log the help message
+ */
+function logHelp() {
+  console.log('Commands:');
+  console.log('  cls, clear: clear the console');
+  console.log('  exit, quit: exit the program');
+  console.log('  help: show this help message');
+  console.log('  c, coin: convert coin list to average gold');
+  console.log('  p, price [rarity]: price an item');
+}
+
 async function main() {
   const { command, rest } = await getCommand();
 
@@ -15,6 +27,10 @@ async function main() {
     case 'quit': {
       closeInput();
       return;
+    }
+    case 'help': {
+      logHelp();
+      break;
     }
     case 'coins':
     case 'coin':
